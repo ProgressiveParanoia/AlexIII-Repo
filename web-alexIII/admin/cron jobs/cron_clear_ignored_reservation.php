945d30c6714df814529771d070b03f9c2986481c
Jed Lyle Fernandez <fernandezjedlyle@gmail.com>
2020-07-07 22:08:46 +0800

    possible fix for real path isssue

--- web-alexIII/admin/cron jobs/cron_clear_ignored_reservation.php
+++ web-alexIII/admin/cron jobs/cron_clear_ignored_reservation.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "../config.php";
+    require_once realpath(dirname(__FILE__)) . "/../config.php";
     $sql_remove = "DELETE FROM reservations WHERE changed_verification = 1 AND verified = 0";
     
     if($statement = mysqli_prepare($link, $sql_remove)){
