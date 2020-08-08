c51f1cc7759ba1956fc5908eaa20054bc493fd30
Jed Lyle Fernandez <jed.fernandez@nstbg.com>
2020-08-09 00:50:24 +0800

    fixed layouting for second stage of delivery

--- web-alexIII/delivery_2.php
+++ web-alexIII/delivery_2.php
@@ -8,12 +8,14 @@
 
   $file_dir = "admin/uploads/delivery_menu/";
   $current_session_id = "";
+  /*
   if(!isset($_COOKIE["delivery_id"])){
               echo "<script>
                     alert('Invalid session Id!');
                     window.location.href = 'http://alexiiirestaurant.com/delivery.php';
                 </script>"; 
   }
+  */
   
   /*
   if(array_key_exists("id", $_GET))
@@ -185,6 +187,7 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/reset.css">
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
     <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/zerogrid.css" type="text/css" media="all">
   <link rel="stylesheet" href="css/responsive.css" type="text/css" media="all">
@@ -205,7 +208,6 @@
       })
     });
 
-
   </script>
   <!--[if lt IE 8]>
        <div style=' clear: both; text-align:center; position: relative;'>
@@ -218,12 +220,54 @@
       <script type="text/javascript" src="js/html5.js"></script>
       <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
   <![endif]-->
+  <style>
+    #stickynav {
+      z-index: 4;
+      overflow: hidden;
+      background-color: #333;
+    }
+
+    #stickynav a {
+      float: left;
+      display: block;
+      color: #f2f2f2;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 17px;
+    }
+
+    #stickynav a:hover {
+      background-color: #ddd;
+      color: black;
+    }
+
+    #stickynav a.active {
+      background-color: #4CAF50;
+      color: white;
+    }
+
+    .content {
+      padding: 16px;
+    }
+
+    .sticky {
+      position: fixed;
+      top: 0;
+      width: 100%;
+    }
+
+    .sticky + .content {
+      padding-top: 60px;
+    }
+  </style>
 </head>
 <body>
 <div class="bg-top">
 <div class="bgr">
   <!--==============================header=================================-->
-    <header>
+    <header></header>
+      
     <div class="zerogrid">
         <center>
           <a href="index.php"><img src="images/AlexLOGO.png" width="150" height="90"></a>
@@ -240,7 +284,7 @@
             <div class="clear"></div>
          </nav>
      -->
-      <div class="topnav" id="myTopnav">
+    <div class="topnav" id="myTopnav">
       <a href="index.php">Home</a>
       <a href="gallery.php">Gallery</a>
       <a href="menu.php">Menu</a>
@@ -253,7 +297,35 @@
       </a>
     </div>
 
+    <div id="stickynav">
+      <a href="index.php">Home</a>
+      <!--
+      <a href="gallery.php">Gallery</a>
+      <a href="menu.php">Menu</a>
+      <a href="contacts.php">Contacts</a>
+      <a href="reservation.php">Reservation</a>
+      <a href="delivery.php" class="active">Delivery</a>
+      <a href="tracker.php">Tracker</a>
+  -->
+    </div>
+
     <script>
+      // When the user scrolls the page, execute myFunction
+      window.onscroll = function() {onStickyScroll()};
+      // Get the navbar
+      var navbar = document.getElementById("stickynav");
+
+      // Get the offset position of the navbar
+      var sticky = navbar.offsetTop;
+
+      // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+      function onStickyScroll() {
+        if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky")
+        } else {
+          navbar.classList.remove("sticky");
+        }
+      }
       function myFunction() {
         var x = document.getElementById("myTopnav");
         if (x.className === "topnav") {
@@ -263,42 +335,80 @@
         }
       }
     </script>
-     </div>  
-    </header>  
+     </div>   
   <!--==============================content================================-->
    <section id="content">
       <div class="zerogrid">
        <div class="block-2 pad-2">
-        <div class="col-5"> 
-         <h3 class="h3">Menu:</h3>
-          <form id="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"method="post">
-            <font color="black">
-            <ul>
+        <div class="col-12"> 
+          <h2>Category</h2>
+       
+          <form id="menu-entry" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"method="post">
+            <!--<ul>-->
             <?php
+              //additional for-loop for categories...
+              // loop 1 = category
+              // loop 2 = category items
+              echo "<div class='form-group row'>";
               for($x = 0; $x < count($menu_data); $x++)
               {
                 $column_data = $menu_data[$x];
                 $id = $column_data[0];
 
-                echo "<li><img src='".$file_dir.$id.".jpg' class='col-sm-1 control-label'></li>";
-                echo"<li><label class='col-sm-1 control-label' name='name' value='".$column_data[2]."'>Name:".$column_data[2]."</li>";
-                echo"<li><label class='col-sm-1 control-label' name='price' value='".$column_data[1]."'>Price:".$column_data[1]."</li>";
-                echo "<button class='col-sm-1 control-label' name='add_item' value='".$id."'>Add to Cart</button>";
-              }    
-            ?>
+                echo "<div class='col-sm-4'>";
+                  //echo "<img src='".$file_dir.$id.".jpg'>";
+                  echo "<image src='".$file_dir."page4-img6.jpg'>";
+                echo "</div>";
+                /*
+                echo "<div class='col-sm-4'>";
+                  echo "<label name='name' value='".$column_data[2]."'><h4>".$column_data[2]."</h4>";
+                echo "</div>";
+                echo "<div class='col-sm-3'>";
+                  echo "<label name='price' value='".$column_data[1]."'><h5>".$column_data[1]."</h5>";
+                echo "</div>";
+                */
+                echo "<div class='col-sm-6'>";
+                    echo "<div class='row'>";
+                        echo "<div class='col-sm-8'>";
+                          echo "<h3>Pritong Tite</h3>";
+                        echo "</div>";
+                        echo "<div class='col-sm-2'>";
+                          echo "<h5>69.00</h3>";
+                        echo "</div>";
+                    echo "</div>";
 
-            </ul>
-          </font>
+                    echo "<div class='row'>";
+                      echo "<div class='col-sm-12'>";
+                        echo "<h5> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                              </h5>";
+                      echo "</div>";                  
+                    echo "</div>";
+                echo "</div>";
+                echo "<div class='col-sm-2'>";
+                  echo "<button type='button' class='btn btn-success' name='add_item' value='".$id."'>Add to Cart</button>";
+                echo "</div>";
+               // echo "<button class='col-sm-4 control-label' name='add_item' value='".$id."'>Add to Cart</button>";
+                /*
+                echo "<li><img src='".$file_dir.$id.".jpg' class='col-sm-1 control-label'></li>";
+                echo"<li><label class='col-sm-4 control-label' name='name' value='".$column_data[2]."'>Name:".$column_data[2]."</li>";
+                echo"<li><label class='col-sm-4 control-label' name='price' value='".$column_data[1]."'>Price:".$column_data[1]."</li>";
+                echo "<button class='col-sm-4 control-label' name='add_item' value='".$id."'>Add to Cart</button>";
+                */
+              }   
+              
+              echo "</div>"; 
+            ?>
+           <!--</ul>-->
          </form>
         </div>
-
+<!-- FIX LAYOUT FOR CARTS
         <div class="block-2 col-3"> 
         <div class="col-4"> 
          <h3 class="h3">Items in Cart:</h3>
           <form id="form" method="post"> 
           <ul>
             <?php
-
+          /*
             $method_name= "item_1";
             $method_idx = 1;
 
@@ -313,14 +423,15 @@
               $method_idx++;
               $method_name = "item_" . $method_idx;
              }
+             */
             ?>
             <li><center><button onclick="myFunction()" type="submit" class="btn btn-round btn-success" name ="submit_delivery">Submit</button></center></li>
             <script>alert("Your session id is "+ <?php echo $current_session_id; ?>);</script>
           </ul>
          </form>
         </div>
-
        </div>
+-->   
       </div>
     </div>
     </section> 
