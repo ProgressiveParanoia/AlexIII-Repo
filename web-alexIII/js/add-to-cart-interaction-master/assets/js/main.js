5dc8685cdf5d94a4eaad3589a884cafc9a0d0f76
Jed Lyle Fernandez <jed.fernandez@nstbg.com>
2020-09-03 12:44:49 +0800

    fixed issue where deleting an itm from cart wont remove it

--- web-alexIII/js/add-to-cart-interaction-master/assets/js/main.js
+++ web-alexIII/js/add-to-cart-interaction-master/assets/js/main.js
@@ -10,12 +10,13 @@
 	}
 
 	class UserEntry{
-		constructor(id, name, number, email, address){
+		constructor(id, name, number, email, address, itemKey){
 			this.id = id;
 			this.name = name;
 			this.number = number;
 			this.email = email;
 			this.address = address;
+			this.itemKey = itemKey;
 		}
 	}
   // Add to Cart Interaction - by CodyHouse.co
@@ -51,7 +52,7 @@
 						for(let i = 0; i < cartSize; i++){
 							let currentKey = parsedCartKeys[i]
 							let parsedEntry = parsedCart[currentKey];
-							let menuEntry = new MenuEntry(parsedEntry[0], parsedEntry[1], parsedEntry[2], parsedEntry[3]);
+							let menuEntry = new MenuEntry(parsedEntry[0], parsedEntry[1], parsedEntry[2], parsedEntry[3], currentKey);
 							userCart.push(menuEntry);	
 							addProduct(menuEntry);
 							updateCartCount(i + 1);
@@ -158,7 +159,7 @@ Util.removeClass(cart[0], 'cd-cart--empty');
 			productId = productId + 1;
 			//let entry = new MenuEntry(productId, "69", "Entry number " + productId, "", "");
   $file_dir = "admin/uploads/delivery_menu/";
-			var productAdded ='<li class="cd-cart__product"><div id="product_entry" class="cd-cart__image" ><a href="#0"><img src="../admin/uploads/delivery_menu/'+entry.id+'.jpg" width="180" height="180" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">'+entry.name+'</a></h3><span class="cd-cart__price">'+entry.price+'.00</span><div class="cd-cart__actions"><button type="submit" class="btn btn-success" name="remove_item" value="'+entry.id+'">Delete</button><div class="cd-cart__quantity"><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
+			var productAdded ='<li class="cd-cart__product"><div id="product_entry" class="cd-cart__image" ><a href="#0"><img src="../admin/uploads/delivery_menu/'+entry.id+'.jpg" width="180" height="180" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">'+entry.name+'</a></h3><span class="cd-cart__price">'+entry.price+'.00</span><div class="cd-cart__actions"><button type="submit" class="btn btn-success" name="remove_item" value="'+entry.itemKey+'">Delete</button><div class="cd-cart__quantity"><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
 			cartList.insertAdjacentHTML('beforeend', productAdded);
 		};
 
